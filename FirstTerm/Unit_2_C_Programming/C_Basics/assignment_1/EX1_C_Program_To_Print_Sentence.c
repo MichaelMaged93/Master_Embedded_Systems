fcab06be2b6ed3811215d49ca776cc4d09307d79
MichaelMaged93 <michaelmaged28893@gmail.com>
2024-02-01 12:48:42 +0200

    the first assignment

---
+++ FirstTerm/Unit_2_C_Programming/C_Basics/assignment_1/EX1_C_Program_To_Print_Sentence.c
@@ -0,0 +1,17 @@
+/*
+ ======================================================================================================================
+ Name        : EX1_C_Program_To_Print_Sentence.c
+ Author      : Michael Maged
+ Created on	 : Jun 31, 2024
+ Description : Unit 2 C-Progtamming >> Assignment 1 C-Basic >> EX1 C Program To Print Sentence using Ansi-style (c99)
+ ======================================================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("C Programming");
+	return 0;
+}
